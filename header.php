1bc461006bfcf9baf5e4900cc8c7d576cb9b407d
Jamie <jamie7williams@gmail.com>
2026-01-05 15:20:19 +0000

    Create footer and header; rename front-page file

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <title><?php bloginfo('name') ?></title>
+  <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+  <header>
+
+  </header>
