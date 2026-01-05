8010362088a06b2875432811a304d14b56a4fcbc
Jamie <jamie7williams@gmail.com>
2026-01-05 17:02:05 +0000

    Register CPTs
    Added CPT registration for: announcements, newsletters, clergy, church groups

---
+++ inc/cpts.php
@@ -0,0 +1,62 @@
+<?php
+
+add_action("init", "parish_register_cpts");
+
+function parish_register_cpts()
+{
+  register_post_type("parish_announcement", array(
+    "labels" => array(
+      "name" => __("Announcements", "catholic-parish"),
+      "singular_name" => __("Announcement", "catholic-parish")
+    ),
+    "description" => __("Parish announcements and notices", "catholic-parish"),
+    "menu_icon" => "dashicons-megaphone",
+    "public" => true,
+    "show_in_rest" => true,
+    "has_archive" => true,
+    "rewrite" => array("slug" => "announcements"),
+    "supports" =>
+    ["title", "editor", "excerpt"],
+  ));
+
+  register_post_type("parish_newsletter", array(
+    "labels" => array(
+      "name" => __("Newsletters", "catholic-parish"),
+      "singular_name" => __("Newsletter", "catholic-parish")
+    ),
+    "description" => __("Parish newsletters", "catholic-parish"),
+    "menu_icon" => "dashicons-media-document",
+    "public" => true,
+    "show_in_rest" => true,
+    "has_archive" => true,
+    "rewrite" => array("slug" => "newsletters"),
+    "supports" => ["title", "custom-fields"],
+  ));
+
+  register_post_type("parish_clergy_member", array(
+    "labels" => array(
+      "name" => __("Clergy", "catholic-parish"),
+      "singular_name" => __("Clergy Member", "catholic-parish")
+    ),
+    "description" => __("Information about clergy members", "catholic-parish"),
+    "menu_icon" => "dashicons-businessperson",
+    "public" => true,
+    "show_in_rest" => true,
+    "rewrite" => array("slug" => "clergy"),
+    "supports" => ["title", "custom-fields", "thumbnail"],
+
+  ));
+
+  register_post_type("parish_church_group", array(
+    "labels" => array(
+      "name" => __("Church Groups", "catholic-parish"),
+      "singular_name" => __("Church Group", "catholic-parish")
+    ),
+    "description" => __("Information about the parish groups", "catholic-parish"),
+    "menu_icon" => "dashicons-groups",
+    "public" => true,
+    "show_in_rest" => true,
+    "rewrite" => array("slug" => "church-groups"),
+    "supports" => ["title", "custom-fields"],
+  ));
+}
