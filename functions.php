9d95b9ff9fbba9b126fcb0da435e20b8dbe473f1
Jamie <jamie7williams@gmail.com>
2026-01-06 13:04:53 +0000

    Enqueue style.css; add comment

--- functions.php
+++ functions.php
@@ -1,7 +1,18 @@
 <?php
 
+// CPTs, taxonomies and meta boxes setup
 $template_dir = get_template_directory();
 
 require_once $template_dir . "/inc/cpts.php";
 require_once $template_dir . "/inc/taxonomies.php";
 require_once $template_dir . "/inc/meta-boxes.php";
+
+// Enqueue styles
+add_action("wp_enqueue_scripts", "parish_enqueue_styles");
+
+function parish_enqueue_styles()
+{
+  $version = wp_get_theme()->get("Version");
+
+  wp_enqueue_style("parish-style", get_stylesheet_uri(), [], $version);
+}
