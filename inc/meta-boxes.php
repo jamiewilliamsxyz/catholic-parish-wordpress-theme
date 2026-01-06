e1c588e31407ca1a2a773679a1822ad5770716eb
Jamie <jamie7williams@gmail.com>
2026-01-06 12:35:25 +0000

    Add nonce verification to meta box inputs

--- inc/meta-boxes.php
+++ inc/meta-boxes.php
@@ -35,6 +35,8 @@ function parish_newsletter_meta_box($post)
 {
   $pdf_file = get_post_meta($post->ID, "pdf_file", true);
 
+  wp_nonce_field("parish_save_newsletter", "parish_newsletter_nonce");
+
 ?>
   <label for="pdf_file">PDF File URL</label><br />
   <input type="url" name="pdf_file" value="<?php echo esc_attr($pdf_file); ?>" />
@@ -46,6 +48,8 @@ function parish_clergy_meta_box($post)
   $email = get_post_meta($post->ID, "email", true);
   $phone = get_post_meta($post->ID, "phone", true);
   $about = get_post_meta($post->ID, "about", true);
+
+  wp_nonce_field("parish_save_clergy", "parish_clergy_nonce");
 
 ?>
   <label for="email">Email</label><br />
@@ -67,6 +71,8 @@ function parish_group_meta_box($post)
   $description = get_post_meta($post->ID, "description", true);
   $meeting_time = get_post_meta($post->ID, "meeting_time", true);
   $location = get_post_meta($post->ID, "location", true);
+
+  wp_nonce_field("parish_save_group", "parish_group_nonce");
 
 ?>
   <label for="leader_name">Leader Name</label><br />
@@ -96,17 +102,101 @@ function parish_save_meta_boxes($post_id)
 {
   if (defined("DOING_AUTOSAVE") && DOING_AUTOSAVE) return;
 
-  if (isset($_POST["pdf_file"])) update_post_meta($post_id, "pdf_file", esc_url_raw($_POST["pdf_file"]));
+  // Newsletter
+  if (
+    isset($_POST["parish_newsletter_nonce"]) &&
+    wp_verify_nonce($_POST["parish_newsletter_nonce"], "parish_save_newsletter")
+  ) {
+    if (isset($_POST["pdf_file"])) {
+      update_post_meta(
+        $post_id,
+        "pdf_file",
+        esc_url_raw($_POST["pdf_file"])
+      );
+    }
+  }
 
-  if (isset($_POST["email"])) update_post_meta($post_id, "email", sanitize_email($_POST["email"]));
-  if (isset($_POST["phone"])) update_post_meta($post_id, "phone", sanitize_text_field($_POST["phone"]));
-  if (isset($_POST["about"])) update_post_meta($post_id, "about", sanitize_textarea_field($_POST["about"]));
+  // Clergy
+  if (
+    isset($_POST["parish_clergy_nonce"]) &&
+    wp_verify_nonce($_POST["parish_clergy_nonce"], "parish_save_clergy")
+  ) {
+    if (isset($_POST["email"])) {
+      update_post_meta(
+        $post_id,
+        "email",
+        sanitize_email($_POST["email"])
+      );
+    }
 
+    if (isset($_POST["phone"])) {
+      update_post_meta(
+        $post_id,
+        "phone",
+        sanitize_text_field($_POST["phone"])
+      );
+    }
 
-  if (isset($_POST["leader_name"])) update_post_meta($post_id, "leader_name", sanitize_text_field($_POST["leader_name"]));
-  if (isset($_POST["leader_email"])) update_post_meta($post_id, "leader_email", sanitize_email($_POST["leader_email"]));
-  if (isset($_POST["leader_phone"])) update_post_meta($post_id, "leader_phone", sanitize_text_field($_POST["leader_phone"]));
-  if (isset($_POST["description"])) update_post_meta($post_id, "description", sanitize_textarea_field($_POST["description"]));
-  if (isset($_POST["meeting_time"])) update_post_meta($post_id, "meeting_time", sanitize_text_field($_POST["meeting_time"]));
-  if (isset($_POST["location"])) update_post_meta($post_id, "location", sanitize_text_field($_POST["location"]));
+    if (isset($_POST["about"])) {
+      update_post_meta(
+        $post_id,
+        "about",
+        sanitize_textarea_field($_POST["about"])
+      );
+    }
+  }
+
+  // Church group
+  if (
+    isset($_POST["parish_group_nonce"]) &&
+    wp_verify_nonce($_POST["parish_group_nonce"], "parish_save_group")
+  ) {
+    if (isset($_POST["leader_name"])) {
+      update_post_meta(
+        $post_id,
+        "leader_name",
+        sanitize_text_field($_POST["leader_name"])
+      );
+    }
+
+    if (isset($_POST["leader_email"])) {
+      update_post_meta(
+        $post_id,
+        "leader_email",
+        sanitize_email($_POST["leader_email"])
+      );
+    }
+
+    if (isset($_POST["leader_phone"])) {
+      update_post_meta(
+        $post_id,
+        "leader_phone",
+        sanitize_text_field($_POST["leader_phone"])
+      );
+    }
+
+    if (isset($_POST["description"])) {
+      update_post_meta(
+        $post_id,
+        "description",
+        sanitize_textarea_field($_POST["description"])
+      );
+    }
+
+    if (isset($_POST["meeting_time"])) {
+      update_post_meta(
+        $post_id,
+        "meeting_time",
+        sanitize_text_field($_POST["meeting_time"])
+      );
+    }
+
+    if (isset($_POST["location"])) {
+      update_post_meta(
+        $post_id,
+        "location",
+        sanitize_text_field($_POST["location"])
+      );
+    }
+  }
 }
