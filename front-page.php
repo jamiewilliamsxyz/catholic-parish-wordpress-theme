debc9d4f05dd6b8a5386abbbf923528252b5b6ef
Jamie <jamie7williams@gmail.com>
2026-01-17 12:34:42 +0000

    Create hero section

--- front-page.php
+++ front-page.php
@@ -1,6 +1,21 @@
 <?php get_header(); ?>
 
 <main>
+  <section
+    class="parish-hero"
+    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/parish-hero-bg-placeholder.jpg');">
+    <header>
+      <h1>
+        <?php bloginfo("name") ?>
+      </h1>
+
+      <div class="parish-description-container">
+        <h2>
+          <?php bloginfo("description") ?>
+        </h2>
+      </div>
+    </header>
+  </section>
 
 </main>
 
