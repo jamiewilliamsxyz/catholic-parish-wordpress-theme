c9cd2c26d6c3edbe51f33f423e8d3e95749754dc
Jamie <jamie7williams@gmail.com>
2026-01-19 17:15:51 +0000

    Combine enqueue styles with enqueue scripts function

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,9 +1,8 @@
 <?php
 
-// Enqueue Styles
-add_action("wp_enqueue_scripts", "parish_enqueue_styles");
+add_action("wp_enqueue_scripts", "parish_enqueue_scripts");
 
-function parish_enqueue_styles()
+function parish_enqueue_scripts()
 {
   $template_uri = get_template_directory_uri();
   $version = wp_get_theme()->get("Version");
@@ -35,15 +34,6 @@ function parish_enqueue_styles()
     "parish-font",
     "https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
   );
-}
-
-// Enqueue Scripts
-add_action("wp_enqueue_scripts", "parish_enqueue_scripts");
-
-function parish_enqueue_scripts()
-{
-  $template_uri = get_template_directory_uri();
-  $version = wp_get_theme()->get("Version");
 
   // Navbar menu script
   wp_enqueue_script(
