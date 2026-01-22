daca99f4576b366a7aafe2b579cdb5770f0c5059
Jamie <jamie7williams@gmail.com>
2026-01-22 10:37:33 +0000

    Wrap post thumbnail container around post thumbnail

--- single.php
+++ single.php
@@ -3,11 +3,12 @@
 <main>
   <article class="parish-single-post">
     <header>
-      <div class="parish-sp-thumbnail-container">
-        <?php if (has_post_thumbnail()) : ?>
+      <?php if (has_post_thumbnail()) : ?>
+        <div class="parish-sp-thumbnail-container">
           <?php the_post_thumbnail(); ?>
-        <?php endif; ?>
-      </div>
+        </div>
+      <?php endif; ?>
+
       <h1><?php the_title(); ?></h1>
       <span class="parish-sp-date-posted">Date posted: <?php echo get_the_date(); ?></span>
     </header>
