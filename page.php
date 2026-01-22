e75319f100d2d93a3c93e1dcc07726a04146cc39
Jamie <jamie7williams@gmail.com>
2026-01-22 10:19:44 +0000

    Create page.php structure

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main class="parish-page-main">
+  <header>
+    <h1><?php the_title(); ?></h1>
+  </header>
+
+  <section>
+    <?php the_content(); ?>
+  </section>
+</main>
+
+<?php get_footer(); ?>
