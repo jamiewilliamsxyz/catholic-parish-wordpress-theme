bb07e92413a9dcce58862568ff007cf68cb63d13
Jamie <jamie7williams@gmail.com>
2026-01-22 12:55:15 +0000

    Style page.php; remove page thumbnail

--- page.php
+++ page.php
@@ -2,17 +2,14 @@
 
 <main class="parish-page-main">
   <header>
-    <?php if (has_post_thumbnail()) : ?>
-      <div class="parish-page-thumbnail-container">
-        <?php the_post_thumbnail(); ?>
-      </div>
-    <?php endif; ?>
     <h1><?php the_title(); ?></h1>
   </header>
 
-  <section>
-    <?php the_content(); ?>
-  </section>
+  <div class="parish-page-content-wrapper">
+    <section class="parish-page-content">
+      <?php the_content(); ?>
+    </section>
+  </div>
 </main>
 
 <?php get_footer(); ?>
