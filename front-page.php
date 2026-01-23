28b20a6efbfe8bd7d9255bdb4a5a87badedc62bf
Jamie <jamie7williams@gmail.com>
2026-01-23 17:28:01 +0000

    Add hero title/card text Customizer options to hero

--- front-page.php
+++ front-page.php
@@ -6,12 +6,12 @@
     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/parish-hero-bg-placeholder.jpg');">
     <header>
       <h1>
-        <?php bloginfo("name") ?>
+        <?php echo esc_html(get_theme_mod("parish_hero_title",  get_bloginfo("name"))); ?>
       </h1>
 
       <div class="parish-description-container">
         <h2>
-          <?php bloginfo("description") ?>
+          <?php echo esc_html(get_theme_mod("parish_hero_card_text",  get_bloginfo("description"))); ?>
         </h2>
       </div>
     </header>
