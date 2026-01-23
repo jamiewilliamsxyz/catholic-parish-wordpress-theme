91b62edb1546b7388a132a48dea5bd4b991c36a2
Jamie <jamie7williams@gmail.com>
2026-01-23 16:19:53 +0000

    Create Customizer section/settings for fonts

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -7,7 +7,7 @@ function parish_register_theme_options($wp_customize)
   // Colours
 
   $wp_customize->add_section(
-    "parish_colours",
+    "parish_colours_section",
     array(
       "title" => __("Colours", "catholic-parish"),
       "description" => esc_html__("Customise the colours used across your site", "catholic-parish"),
@@ -27,8 +27,80 @@ function parish_register_theme_options($wp_customize)
     "parish_accent_colour",
     array(
       "label" => __("Accent Colour", "catholic-parish"),
-      "section" => "parish_colours",
+      "section" => "parish_colours_section",
       "type" => "color"
     )
   );
+
+  // Typography
+
+  $wp_customize->add_section(
+    "parish_typography_section",
+    array(
+      "title" => __("Typography", "catholic-parish"),
+      "description" => esc_html__("Select fonts for headings and body text across your site", "catholic-parish"),
+      "priority" => 102
+    )
+  );
+
+  $wp_customize->add_setting(
+    "parish_heading_font",
+    array(
+      "default" => "parish-lora-font",
+      "sanitize_callback" => "parish_sanitise_font_choice"
+    )
+  );
+
+  $wp_customize->add_control(
+    "parish_heading_font",
+    array(
+      "label" => __("Heading Font", "catholic-parish"),
+      "section" => "parish_typography_section",
+      "type" => "select",
+      "choices" =>
+      array(
+        "parish-lora-font" => "Lora",
+        "parish-merriweather-font" => "Merriweather",
+        "parish-crimson-text-font" => "Crimson Text",
+        "parish-inter-font" => "Inter",
+        "parish-roboto-font" => "Roboto"
+      )
+    )
+  );
+
+  $wp_customize->add_setting(
+    "parish_body_font",
+    array(
+      "default" => "parish-lora-font",
+      "sanitize_callback" => "parish_sanitise_font_choice"
+    )
+  );
+
+  $wp_customize->add_control(
+    "parish_body_font",
+    array(
+      "label" => __("Body Font", "catholic-parish"),
+      "section" => "parish_typography_section",
+      "type" => "select",
+      "choices" =>
+      array(
+        "parish-lora-font" => "Lora",
+        "parish-merriweather-font" => "Merriweather",
+        "parish-crimson-text-font" => "Crimson Text",
+        "parish-inter-font" => "Inter",
+        "parish-roboto-font" => "Roboto"
+      )
+    )
+  );
+}
+
+// Sanitise callback for font selects
+
+function parish_sanitise_font_choice($input, $setting)
+{
+  $valid = $setting->manager->get_control($setting->id)->choices;
+  if (array_key_exists($input, $valid)) {
+    return $input;
+  }
+  return $setting->default;
 }
