2f26ddb46264526f010a89ffb151e11d0bb6e917
Jamie <jamie7williams@gmail.com>
2026-01-27 13:09:39 +0000

    Create/style content archive for newsletter CPT

--- inc/cpt-archives.php
+++ inc/cpt-archives.php
@@ -6,4 +6,8 @@ function parish_cpt_archives_posts_per_page($query)
   if (!is_admin() && $query->is_main_query() && $query->is_post_type_archive("parish_announcement")) {
     $query->set("posts_per_page", 6);
   }
+
+  if (!is_admin() && $query->is_main_query() && $query->is_post_type_archive("parish_newsletter")) {
+    $query->set("posts_per_page", 8);
+  }
 }
---
+++ template-parts/content/content-archive-parish_newsletter.php
@@ -0,0 +1,15 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class("parish-archive-card"); ?>>
+  <div class="parish-archive-card-content">
+    <span><?php echo get_the_date(); ?></span>
+    <h2><?php the_title(); ?></h2>
+
+    <?php
+    $post_id = get_the_ID();
+    $post_newsletter_url = get_post_meta($post_id, "pdf_file", true);
+
+    if ($post_newsletter_url) :
+    ?>
+      <a class="parish-content-archive-newsletter-link" href="<?php echo esc_url($post_newsletter_url); ?>" target="_blank" rel="noopener">Download</a>
+    <?php endif; ?>
+  </div>
+</article>
