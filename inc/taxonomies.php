0fa73f52a529743685192ba68734468c2349967a
Jamie <jamie7williams@gmail.com>
2026-02-03 14:49:13 +0000

    Refactor group type taxonomy; create staff type taxonomy

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -4,16 +4,47 @@ add_action("init", "parish_register_taxonomies");
 
 function parish_register_taxonomies()
 {
+  $church_group_type_labels = array(
+    "name" => _x("Church Group Types", "taxonomy general name", "catholic-parish-wordpress-theme"),
+    "singular_name" => _x("Church Group Type", "taxonomy singular name", "catholic-parish-wordpress-theme"),
+    "search_items" => __("Search Church Group Types", "catholic-parish-wordpress-theme"),
+    "all_items" => __("All Church Group Types", "catholic-parish-wordpress-theme"),
+    "edit_item" => __("Edit Church Group Type", "catholic-parish-wordpress-theme"),
+    "update_item" => __("Update Church Group Type", "catholic-parish-wordpress-theme"),
+    "add_new_item" => __("Add New Church Group Type", "catholic-parish-wordpress-theme"),
+    "new_item_name" => __("New Church Group Type Name", "catholic-parish-wordpress-theme"),
+    "not_found" => __("No Church Group Types found", "catholic-parish-wordpress-theme"),
+    "menu_name" => __("Church Group Types", "catholic-parish-wordpress-theme"),
+  );
+
   register_taxonomy("parish_church_group_type", "parish_church_group", array(
-    "labels" => array(
-      "name" => __("Group Types", "catholic-parish-wordpress-theme"),
-      "singular_name" => __("Group Type", "catholic-parish-wordpress-theme")
-    ),
+    "labels" => $church_group_type_labels,
     "public" => true,
-    "description" => "Used to categorise church groups by type",
-    "show_in_rest" => true,
+    "description" => __("Used to categorise church groups e.g. Youth, Choir, Bible Study", "catholic-parish-wordpress-theme"),
     "show_admin_column" => true,
     "query_var" => true,
-    "rewrite" => ["slug" => "church_group_type"],
+    "rewrite" => array("slug" => "church_group_type"),
+  ));
+
+  $parish_staff_type_labels = array(
+    "name" => _x("Parish Staff Types", "taxonomy general name", "catholic-parish-wordpress-theme"),
+    "singular_name" => _x("Parish Staff Type", "taxonomy singular name", "catholic-parish-wordpress-theme"),
+    "search_items" => __("Search Parish Staff Types", "catholic-parish-wordpress-theme"),
+    "all_items" => __("All Parish Staff Types", "catholic-parish-wordpress-theme"),
+    "edit_item" => __("Edit Parish Staff Type", "catholic-parish-wordpress-theme"),
+    "update_item" => __("Update Parish Staff Type", "catholic-parish-wordpress-theme"),
+    "add_new_item" => __("Add New Parish Staff Type", "catholic-parish-wordpress-theme"),
+    "new_item_name" => __("New Parish Staff Type Name", "catholic-parish-wordpress-theme"),
+    "not_found" => __("No Parish Staff Types found", "catholic-parish-wordpress-theme"),
+    "menu_name" => __("Parish Staff Types", "catholic-parish-wordpress-theme"),
+  );
+
+  register_taxonomy("parish_staff_type", "parish_staff_member", array(
+    "labels" => $parish_staff_type_labels,
+    "public" => true,
+    "description" => __("Used to categorise parish staff members e.g. Clergy or Non-Clergy", "catholic-parish-wordpress-theme"),
+    "show_admin_column" => true,
+    "query_var" => true,
+    "rewrite" => array("slug" => "staff_type"),
   ));
 }
